f86bee6c54b825e7f1fdf1e0e0f7acf4fb88d752
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-09-28 11:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
-
+	e.preventDefault();
+let form  = document.querySelector("#form1");
+let name = document.querySelectorAll("input");
+let fname = name[0].value, lname = name[1].value;
+	
+	
+	alert(fname+" "+lname);
+});
 }
