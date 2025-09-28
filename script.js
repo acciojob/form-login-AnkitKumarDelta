446ec0c4f5be692454d17212a0df22c4948eed04
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-09-28 13:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function getFormvalue(e) {
-    //Write your code here
+// e.preventDefault();
+// let form  = document.querySelector("#form1");
+// let name = document.querySelectorAll("input");
+// let fname = name[0].value, lname = name[1].value;
+// alert(fname + " " + lname);
+
 	e.preventDefault();
-let form  = document.querySelector("#form1");
-let name = document.querySelectorAll("input");
-let fname = name[0].value, lname = name[1].value;
-	
-	
-	alert(fname+" "+lname);
-});
+      let form  = document.querySelector("#form1");
+      let name = form.querySelectorAll("input");
+      let fname = name[0].value;
+      let lname = name[1].value;
+      alert(fname + " " + lname);
 }
+
+
